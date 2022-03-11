07c174683c9a68f4ed49196b2c537f75d161c4c7
Andre Costa <andre2396costa@gmail.com>
2022-03-11 12:52:28 -0300

    Fix netlify erro build (Line 1:10:  type is defined but never used) MovieStar

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -2,7 +2,6 @@ import './styles.css';
 import { ReactComponent as StarFull } from "assets/img/star-full.svg";
 import { ReactComponent as StarHalf } from "assets/img/star-half.svg";
 import { ReactComponent as StarEmpty } from "assets/img/star-empty.svg";
-import { type } from '@testing-library/user-event/dist/type';
 
 type Props = {
     score: number;
